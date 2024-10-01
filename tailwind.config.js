a83bc3d3975c258cd904ba33b8edf90b020a2055
Michelstuder <107350826+Michelstuder@users.noreply.github.com>
2024-10-01 09:43:18 +0200

    Initialized react app with typescript and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",  // React-Dateien
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
